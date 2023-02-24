20a6d6b8c3fb7c20513a1c067515f32f27855e45
Heri Risnanto <heri.risnanto@gmail.com>
2023-02-24 17:58:01 +0700

    chore: install & setup chakra ui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import { createPinia, PiniaVuePlugin } from 'pinia'
+import Chakra, { CThemeProvider, CReset } from '@chakra-ui/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -7,9 +8,10 @@ import router from './router'
 import './assets/main.css'
 
 Vue.use(PiniaVuePlugin)
+Vue.use(Chakra)
 
 new Vue({
   router,
   pinia: createPinia(),
-  render: (h) => h(App)
+  render: (h) => h(CThemeProvider, [h(CReset), h(App)])
 }).$mount('#app')
---
+++ src/views/ListHargaUdang.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { CBox, CButton } from '@chakra-ui/vue'
+</script>
+
+<template>
+  <main>
+    <c-box>
+      <c-button>
+        Chakra Consumed! ⚡️
+      </c-button>
+    </c-box>
+  </main>
+</template>
