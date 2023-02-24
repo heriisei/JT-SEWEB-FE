8b12ffe8c38723f864c780381ca6bdd4bbdebeee
Heri Risnanto <heri.risnanto@gmail.com>
2023-02-25 06:33:38 +0700

    chore: install autocomplete select component

--- src/views/ListHargaUdang.vue
+++ src/views/ListHargaUdang.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { CBox, CButton } from '@chakra-ui/vue'
+import Multiselect from 'vue-multiselect'
+
+const value = ref('')
+const options = ['Select option', 'options', 'selected', 'multiple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched']
 </script>
 
 <template>
@@ -9,5 +14,13 @@ import { CBox, CButton } from '@chakra-ui/vue'
         Chakra Consumed! ⚡️
       </c-button>
     </c-box>
+    <div>
+      <label class="typo__label">Single select</label>
+      <multiselect v-model="value" :options="options" :searchable="false" :close-on-select="false" :show-labels="false"
+        placeholder="Pick a value"></multiselect>
+      <pre class="language-json"><code>{{ value }}</code></pre>
+    </div>
   </main>
 </template>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
