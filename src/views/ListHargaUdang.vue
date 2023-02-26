6c6b47909cbcf4d928847519a634d938cc7a3758
Heri Risnanto <heri.risnanto@gmail.com>
2023-02-26 17:30:34 +0700

    fix: container & wrapper padding

--- src/views/ListHargaUdang.vue
+++ src/views/ListHargaUdang.vue
@@ -9,5 +9,20 @@ import dayjs from 'dayjs';
     <div class="wrapper-break bg-#fff">
       <filter-shrimp-prices-by-location></filter-shrimp-prices-by-location>
     </div>
+    <section class="wrapper">
+      <h2 class="sr-only">Promotional content</h2>
+      <div class="flex flex-col md:flex-row gap-.875rem">
+        <a href="https://api.whatsapp.com/send?phone=081325514194&amp;text=Halo%20JALA,%20saya%20ingin%20melakukan%20uji%20penyakit%20udang"
+          target="_blank"><img src="https://drive.google.com/uc?id=15_wFMILzgqt-NZ5A_lxqlonQVYSNjw2_&amp;export=media"
+            alt="box-img" title="Uji Penyakit Udang Anda di LabUdang
+      Hubungi tim Jala untuk menguji penyakit udang Anda di LabUdang Jala" class="img-responsive m-t-sm"
+            style="width: 100%;"></a>
+        <a href="https://api.whatsapp.com/send?phone=081325514194&amp;text=Halo%20JALA,%20saya%20berencana%20melakukan%20panen%20udang"
+          target="_blank"><img src="https://drive.google.com/uc?id=1YIyOuyXFoJ5e6FFtRKnnX_iKUuRBk7nX&amp;export=media"
+            alt="box-img" title="Jala Harvest
+      Shrimp harvest is paid fast with JALA Harvest, harvest-pay!" class="img-responsive m-t-sm"
+            style="width: 100%;"></a>
+      </div>
+    </section>
   </main>
 </template>
