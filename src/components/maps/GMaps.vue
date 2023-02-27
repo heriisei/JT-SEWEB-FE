ef0bac6069e16a2c2b7fca43d41d652399a3b363
Heri Risnanto <heri.risnanto@gmail.com>
2023-02-27 11:19:59 +0700

    feat: setup (initial) GMaps components

---
+++ src/components/maps/GMaps.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Loader } from '@googlemaps/js-api-loader'
+
+const props = defineProps<{
+  locations: Record<string, any>[];
+}>()
+
+type Libraries = ("drawing" | "geometry" | "localContext" | "marker" | "places" | "visualization")[];
+const apiOptions = {
+  apiKey: `${import.meta.env.VITE_GMAPS_API_KEY}`,
+  version: 'beta',
+  libraries: ['marker'] as Libraries,
+}
+const map = ref()
+const loader = new Loader(apiOptions);
+
+loader.load().then(() => {
+  // console.log('Maps JS API Loaded');
+  displayMap()
+  addMarkers()
+});
+
+const displayMap = () => {
+  const mapOptions = {
+    center: { lat: -33.860664, lng: 151.208138 },
+    zoom: 14,
+    mapId: 'DEMO_MAP_ID'
+  };
+  const mapEl = document.querySelector('#map')
+  //@ts-ignore
+  map.value = new google.maps.Map(mapEl, mapOptions);
+  return map.value
+}
+
+const addMarkers = () => {
+  const markers: Record<string, any> = []
+  props.locations.forEach((loc: Record<string, any>, index: number) => {
+    // Create advance map marker with HTML element
+    const priceMarker = document.createElement('div');
+    priceMarker.className = 'price-marker';
+    priceMarker.textContent = loc.name;
+
+    const markerOptions = {
+      map: map.value,
+      position: props.locations[index].coordinate,
+      content: priceMarker,
+    }
+    //@ts-ignore
+    const marker: Record<string, any> = new google.maps.marker.AdvancedMarkerView(markerOptions)
+    markers.push(marker)
+  })
+  return markers
+}
+
+onMounted(() => {
+  // displayMap()
+})
+</script>
+<template>
+  <div>
+    <div id="map" ref="mapEl"></div>
+  </div>
+</template>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 300px;
+}
+</style>
+
+<style>
+.price-marker {
+  background-color: #4285F4;
+  border-radius: 8px;
+  color: #FFFFFF;
+  font-size: 14px;
+  padding: 10px 15px;
+  position: relative;
+}
+
+.price-marker::after {
+  content: "";
+  position: absolute;
+  left: 50%;
+  top: 100%;
+  transform: translate(-50%, 0);
+  width: 0;
+  height: 0;
+  border-left: 8px solid transparent;
+  border-right: 8px solid transparent;
+  border-top: 8px solid #4285F4;
+}
+
+[class$=api-load-alpha-banner] {
+  display: none;
+}
+</style>
--- src/views/ListHargaUdang.vue
+++ src/views/ListHargaUdang.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { CBox, CButton } from '@chakra-ui/vue'
-import FilterShrimpPricesByLocation from '@/components/filter/FilterShrimpPricesByLocation.vue';
 import dayjs from 'dayjs';
+import FilterShrimpPricesByLocation from '@/components/filter/FilterShrimpPricesByLocation.vue';
+import GMaps from '@/components/maps/GMaps.vue';
 </script>
 
 <template>
@@ -15,14 +17,32 @@ import dayjs from 'dayjs';
         <a href="https://api.whatsapp.com/send?phone=081325514194&amp;text=Halo%20JALA,%20saya%20ingin%20melakukan%20uji%20penyakit%20udang"
           target="_blank"><img src="https://drive.google.com/uc?id=15_wFMILzgqt-NZ5A_lxqlonQVYSNjw2_&amp;export=media"
             alt="box-img" title="Uji Penyakit Udang Anda di LabUdang
-      Hubungi tim Jala untuk menguji penyakit udang Anda di LabUdang Jala" class="img-responsive m-t-sm"
+                  Hubungi tim Jala untuk menguji penyakit udang Anda di LabUdang Jala" class="img-responsive m-t-sm"
             style="width: 100%;"></a>
         <a href="https://api.whatsapp.com/send?phone=081325514194&amp;text=Halo%20JALA,%20saya%20berencana%20melakukan%20panen%20udang"
           target="_blank"><img src="https://drive.google.com/uc?id=1YIyOuyXFoJ5e6FFtRKnnX_iKUuRBk7nX&amp;export=media"
             alt="box-img" title="Jala Harvest
-      Shrimp harvest is paid fast with JALA Harvest, harvest-pay!" class="img-responsive m-t-sm"
+                Shrimp harvest is paid fast with JALA Harvest, harvest-pay!" class="img-responsive m-t-sm"
             style="width: 100%;"></a>
       </div>
+    </section>
+    <section class="wrapper">
+      <h2>persebaran harga udang size 100</h2>
+      <g-maps :locations="[
+        {
+          name: 'operaHouse',
+          coordinate: { lat: -33.8567844, lng: 151.213108 }
+        },
+        {
+          name: 'gaZoo',
+          coordinate: { lat: -33.8472767, lng: 151.2188164 }
+        },
+        {
+          name: 'manlyBeach',
+          coordinate: { lat: -33.8209738, lng: 151.2563253 }
+        }
+      ]">
+      </g-maps>
     </section>
   </main>
 </template>
