15503224d9159d2b78fa0e27db2923f9074938fa
Heri Risnanto <heri.risnanto@gmail.com>
2023-02-28 10:47:22 +0700

    feat: update locale strings

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,4 +1,9 @@
 export default {
   locationChoose: 'Choose location',
   locationNA: 'Location not available',
+  promotionalContent: 'promotional content',
+  shrimpPrizeMapTitle: 'distribution of shrimp prices by size',
+  month: 'month',
+  minggu: 'week',
+  new: 'new',
 };
--- src/locales/id/index.ts
+++ src/locales/id/index.ts
@@ -1,4 +1,9 @@
 export default {
   locationChoose: 'Pilih lokasi',
   locationNA: 'Lokasi tidak tersedia',
+  promotionalContent: 'konten promosi',
+  shrimpPrizeMapTitle: 'persebaran harga udang size',
+  month: 'bulan',
+  minggu: 'week',
+  new: 'baru',
 };
